43843236f5685dca8ac504db724149ce56e5e04d
Lukas Bachlechner <lukas.bachlechner@marqably.com>
2024-08-03 12:18:19 +0200

    feat: add first flame stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flame/layout.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  final game = FlameGame();
-  runApp(GameWidget(game: game));
+void main() async {
+  runApp(const GameWrapper());
+
+  await Flame.device.fullScreen();
+}
+
+class GameWrapper extends StatelessWidget {
+  const GameWrapper({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: GameWidget(
+            game: BattleshipGame(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BattleshipGame extends FlameGame {
+  BattleshipGame();
+
+  @override
+  void onLoad() {
+    add(AlignComponent(
+      child: TextComponent(text: 'Battleships', position: Vector2.zero()),
+      alignment: Anchor.center,
+    ));
+  }
 }
