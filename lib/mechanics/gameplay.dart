21cc7c5f56aaea459e1bbdb5666dcf1b890bcd09
Nidhil Suresh <nidhil.sureshbabu@marqably.com>
2024-08-03 17:29:50 +0200

    Added power shots

--- lib/mechanics/gameplay.dart
+++ lib/mechanics/gameplay.dart
@@ -2,15 +2,22 @@ import 'package:flutter_hackathon/models/board.model.dart';
 import 'package:flutter_hackathon/models/game.model.dart';
 import 'package:flutter_hackathon/models/helper/coordinates.model.dart';
 import 'package:flutter_hackathon/models/helper/hits.model.dart';
+import 'package:flutter_hackathon/models/helper/shiptype.model.dart';
 
 class GamePlay {
   const GamePlay({required this.game});
   final Game game;
 
-  CellHit hitCell(Coordinates coordinates, HitType hitType) {
-    final CellHit cellHitImpact = opponentBoard.hitCell(coordinates, hitType);
-
-    return cellHitImpact;
+  List<CellHit> hitCell(Coordinates coordinates, PowerShots? powerShot,
+      ShipOrientation? direction) {
+    if (powerShot == null) {
+      final CellHit cellHitImpact =
+          opponentBoard.hitCell(coordinates, HitType.damage);
+      return [cellHitImpact];
+    } else {
+      return opponentBoard.hitCellWithPowerShot(
+          coordinates, powerShot, direction);
+    }
   }
 
   nextTurn() {
