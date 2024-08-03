0d8e79dafddcb7b6f971ba32d9dd18e9073af8bf
Jacob Aschenwald <jacob.aschenwald@gmail.com>
2024-08-03 15:25:57 +0200

    feat: add ship selection component

---
+++ lib/components/ship_selection_component.dart
@@ -0,0 +1,32 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+import '../models/ship.model.dart';
+import 'ship_component.dart';
+
+Color colorOfState(ShipPlacementState state) {
+  switch (state) {
+    case ShipPlacementState.available:
+      return Colors.green;
+    case ShipPlacementState.placing:
+      return Colors.yellow;
+    case ShipPlacementState.placed:
+      return Colors.red;
+  }
+}
+
+class ShipSelectionComponent extends RectangleComponent {
+  ShipSelectionComponent({
+    required this.ship,
+    required Vector2 position,
+    this.state = ShipPlacementState.available,
+  }) : super(
+            size: sizeOfShip(ship),
+            position: position,
+            paint: Paint()..color = colorOfState(state));
+
+  final Ship ship;
+  final ShipPlacementState state;
+}
+
+enum ShipPlacementState { available, placing, placed }
